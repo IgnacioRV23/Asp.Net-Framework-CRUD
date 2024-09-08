95fdbe25c2d3c3129459a28fc62feeb047dd5b8c
Ignacio Rodriguez <nacho.rv2304@gmail.com>
2024-09-07 20:05:29 -0600

    First commit of project.

---
+++ Model/Products.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net_Framework_CRUD.Model
+{
+    public class Products
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+        public void GetRows()
+        {
+            DataTable Products = new DataTable();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open(); // Opens a connection to the database
+
+                    // Creates a SqlCommand to execute the stored procedure "Products_GetRows"
+                    SqlCommand sqlComm = new SqlCommand("Products_GetRows", conn);
+
+                    // Specifies that the command is a stored procedure
+                    sqlComm.CommandType = CommandType.StoredProcedure; 
+
+                    // Fills the DataTable 'Products' with the result set of the stored procedure
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlComm);
+                    adapter.Fill(Products);
+
+                    // Closes the database connection
+                    conn.Close(); 
+                }
+            }
+            catch { }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Views/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Asp.Net_Framework_CRUD.Model;
+
+namespace Asp.Net_Framework_CRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Products p = new Products();
+
+            p.GetRows();
+        }
+    }
+}
